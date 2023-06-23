aecfaf52ce17876f75c2e0b3ea36ed1f69865482
LaBlaugrana <aayush.dubey.21cse@bmu.edu.in>
2023-06-23 15:56:22 +0530

    onboarding

---
+++ android/app/src/main/kotlin/com/example/asdzcsdf/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.asdzcsdf
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/splash_Screen.dart
@@ -0,0 +1,34 @@
+// import 'package:asdzcsdf/main.dart';
+// import 'package:flutter/material.dart';
+//
+// class SplashScreen extends StatelessWidget {
+//   const SplashScreen({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SafeArea(
+//         child: Container(
+//           // decoration: const BoxDecoration(
+//           //   image: DecorationImage(
+//           //     image: AssetImage(splashImg),
+//           //     fit: BoxFit.cover,
+//           //   ),
+//           // ),
+//           padding: EdgeInsets.fromLTRB(0, 200, 0, 100),
+//           child: Stack(
+//             alignment: Alignment.center,
+//             children: const [
+//               Positioned(
+//                 child: Image(
+//                   image: AssetImage(splashImg),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+//
